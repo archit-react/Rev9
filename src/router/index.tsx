3d90b1e0e9e697e181d2698db87b651c8ff4d1e4
Archit <113596573+archit-react@users.noreply.github.com>
2025-10-24 09:36:59 +0530

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,8 +2,8 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 
 import Dashboard from "@/pages/dashboard/dashboard";
 import Users from "@/pages/Users";
-import Settings from "@/pages/setting";
-import Login from "@/pages/logi";
+import Settings from "@/pages/settings";
+import Login from "@/pages/login";
 import Signup from "@/pages/signup";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import AppLayout from "@/layout/AppLayout"; // 
